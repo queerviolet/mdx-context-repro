6a949fa597afbb91d0c553e11f8e36479d2ffaf7
Ashi Krishnan <queerviolet@github.com>
2019-07-25 16:09:39 -0400

    Remove unified-ui provider.

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import {MDXProvider} from '@mdx-js/tag'
-import {Provider} from 'unified-ui'
+// import {MDXProvider} from '@mdx-js/tag'
+import {Provider, MDXProvider} from 'unified-ui'
 
 export default ({Component, pageProps}) => (
   <MDXProvider components={{ p() { return 'Hello?' }}}>
-    <Provider>
-      <Component {...pageProps} />
-    </Provider>
+    <Component {...pageProps} />
   </MDXProvider>
 )
